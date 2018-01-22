44b7f65676de270495599dc7dc777cf37ba98d83
dnzengou <desire.yavro@gmail.com>
2018-01-23 00:09:48 +0100

    updated files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-var $input = $('input');
-var $LastName = $('#lastName');
-
 $(document).ready(function() {
-    var client = algoliasearch('WJAYX27JB5', '7811aa9e37d97771767ce690b7331f92');
+    var client = algoliasearch('#', '#');
     var index = client.initIndex('GSK_Employees_information');
 
     $('#typeahead-algolia').typeahead(null, {
